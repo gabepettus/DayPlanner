5cddcdb095bef6acab41798375e03c8c3f222761
gdpettus <gabe.pettus@gmail.com>
2019-10-09 22:42:45 -0600

    stubbed in css, sudocode outline of approach

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 $(document).ready(function() {
 
-  let now = moment().format('MMMM Do YYYY, h:mm:ss a');
+  // need to put in a timeInterval to update clock dynamically
+  // Bonus need to navigate to past future days - calendar options
+  // get times from moment
+  const let = moment().format('MMMM Do YYYY, h:mm:ss a');
+  // returns current hour
+  const nowHour = moment().format('H');
+  // logic to check for hour roll over call updaterowcolor for each row
+  // end timer
 
-  $("#now").html("<h1>Day Planner!</h1>");
-  $("#now").append(now); 
+  // draw calendar
+
+  // loop through 9 to 5 
+  // add hour row : need id to update
+  // need time element, meeting, crud context sensitive buttons icons
+  
+  // need eventListener on entire calendar looking for onclick to edit?
+
+  //option to save to local memory
+
+
+  // function to update local memory
+  // function to update row color
+  let updateColor = updateRowColor (rowId) {
+
+  }
+
+  // test long strings
+  // test html injection
+
+  
+
+
+
+
+
+  $("#planner").html("<h1>Day Planner!</h1>");
+  $("#planner").append(now); 
 });
