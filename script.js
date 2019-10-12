bdb6b7a0172560a72cbcf58e48ff4a8577258de1
gdpettus <gabe.pettus@gmail.com>
2019-10-12 12:03:12 -0600

    saved entries to local storage

--- script.js
+++ script.js
@@ -1,36 +1,59 @@
 $(document).ready(function() {
-
+  
   // need to put in a timeInterval to update clock dynamically
   // Bonus need to navigate to past future days - calendar options
   // get times from moment
   // const now = moment().format('MMMM Do YYYY, h:mm:ss a');
   // returns current hour
-
-  // const nowHour24 = moment().format('H');
-  const nowHour24 = 14;
-  // const nowHour12 = moment().format('h');
-  const nowHour12 = 2;
+  
+  // commented out for test in non-standard hours
+  const nowHour24 = moment().format('H');
+  const nowHour12 = moment().format('h');
+  // const nowHour24 = 13;
+  // const nowHour12 = 1;
+  
+  // let planTextArr = null;
+  // initialize empty array of length of day 
 
   const test = true;
+  // Get stored todos from localStorage
+  // Parsing the JSON string to an object
+  let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+
+  console.log(storedPlans);
+
+  // If plans were retrieved from localStorage, update the plan array to it
+  if (storedPlans !== null) {
+    planTextArr = storedPlans;
+  } else {
+    // this should only occur on first time the app is loaded in the browser
+    // helpfully remind user that lunch is important
+    planTextArr = new Array(9);
+    planTextArr[4] = "Picnic lunch outside";
+  }
+
+  console.log("full array of plned text",planTextArr);
 
   // logic to check for hour roll over call updaterowcolor for each row
   // end timer
-
+  
   // draw calendar
   // set variable referencing ul planner element
   let $plannerUl = $('#planner');
   // clear existing elements
   $plannerUl.empty();
 
-  // let hourArr = [9,10,11,12,1,2,3,4,5];
+  let hourArr = [9,10,11,12,1,2,3,4,5];
   // hourArr.forEach((hour) => 
 
   // Render a new li for each hour in the planner
-  console.log(nowHour12);
+  if (test) { console.log("current time",nowHour12); }
 
   for (let hour = 9; hour <= 17; hour++) {
-    // create list item container for the row
+    // index for array use offset from hour
+    let index = hour - 9;
 
+    // create list item container for the row
     let $hourLi = $('<li>');
     //give item idex which cooresponds with the start of the hour
     $hourLi.attr('hour-index',hour);
@@ -42,20 +65,23 @@ $(document).ready(function() {
     $timeBoxSpn.attr('class','timeBox');
     $timeBoxSpn.text(`${hour}:00`);
 
-    let $dailyPlanSpn = $('<span>');
+    let $dailyPlanSpn = $('<input>');
     // can use this to get value
     //TODO convert this to an icon with a mouse over
+    $dailyPlanSpn.attr('id',`input-${index}`);
+    $dailyPlanSpn.attr('type','text');
     $dailyPlanSpn.attr('class','dailyPlan');
-    $dailyPlanSpn.text('planning text goes here');
+    // access correct index 
+    $dailyPlanSpn.val( planTextArr[index] );
 
     // create save button element
     let $saveBtn = $('<button>');
+    $saveBtn.attr('btn-id',index);
     // can use this to get value
     //TODO convert this to an icon with a mouse over
     $saveBtn.text('Save');
 
     //build row
-    console.log("here");
     $hourLi.append($timeBoxSpn);
     $hourLi.append($dailyPlanSpn);
     $hourLi.append($saveBtn);
@@ -89,6 +115,29 @@ $(document).ready(function() {
       $hourRow.css("background-color","red")
     }
   };
+
+  // conclick function to listen for user clicks on plan area
+  $(document).on('click','button', function(event) {
+  //  $(".dailyPlan").submit( function(event) {
+    event.preventDefault();  
+
+    if (test) { console.log('click pta before '+ planTextArr); }
+
+    let $index = $(this).attr('btn-id');
+
+    let inputId = '#input-'+$index;
+    let $value = $(inputId).val();
+
+    planTextArr[$index] = $value;
+
+    if (test) { console.log('this ', $value); }
+    if (test) { console.log('value ', $value); }
+    if (test) { console.log('index ', $index); }
+    if (test) { console.log('click pta after '+ planTextArr); }
+
+    localStorage.setItem("storedPlans", JSON.stringify(planTextArr));
+  });  
+
 
   // test long strings
   // test html injection
